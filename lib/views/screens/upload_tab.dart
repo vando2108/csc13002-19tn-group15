19f48a1d202056e72906af89b111bdbc2f62ddfb
hieuphan.alienx <hieuphan@alienxspace.com>
2021-12-17 21:45:45 +0700

    upload

--- lib/views/screens/upload_tab.dart
+++ lib/views/screens/upload_tab.dart
@@ -2,45 +2,84 @@ import 'package:flashare/views/widgets/rounded_input_field.dart';
 import 'package:flutter/material.dart';
 
 class UploadTab extends StatefulWidget {
-  const UploadTab({ Key? key }) : super(key: key);
+  const UploadTab({Key? key}) : super(key: key);
 
   @override
   _UploadTabState createState() => _UploadTabState();
 }
 
-class _UploadTabState extends State<UploadTab> {
-  TextEditingController _searchController = TextEditingController();
-  bool _isSearching = false;
-  String searchQuery = "Search_query";
+class _UploadTabState extends State<UploadTab> with SingleTickerProviderStateMixin{
+  late TabController _tabController;
+  int _currentTab = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 3, vsync: this);
+    _tabController.index = _currentTab;
+    _tabController.addListener(() {
+      this.setState(() {
+        _currentTab = _tabController.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 50),
-          _searchBar(),
-          const SizedBox(height: 24),
-          _renderItem(),
-        ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          leading:
+            _searchBar(),
+          bottom: TabBar(
+            controller: _tabController,
+            indicatorColor: Color(0x4285F4),
+            tabs: [
+              Tab(text: 'Posted'),
+              Tab(text: 'In Progress',),
+              Tab(text: 'Sent',),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+          _postedView(),
+          _inProgressView(),
+          _sentView(),
+        ]),
       ),
     );
   }
 
   Widget _searchBar() {
-    return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RoundedInputField(
-                    hintText: "Search",
-                    icon: Icons.search,
-                    onChanged: (value) {}),
-              ],
-            );
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RoundedInputField(
+              hintText: "Search", icon: Icons.search, onChanged: (value) {}),
+        ],
+      ),
+    );
   }
 
-  Widget _renderItem() {
+  Widget _postedView() {
     return Container();
   }
-  
+
+  Widget _inProgressView() {
+    return Container();
+  }
+
+  Widget _sentView() {
+    return Container();
+  }
 }
